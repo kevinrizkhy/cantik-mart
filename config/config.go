46c83e89c8194c72c46d306b619cfc65854f3296
kevinrizkhy <kevin.rizkhy85@gmail.com>
2018-06-14 22:49:31 +0700

    2

--- config/config.go
+++ config/config.go
@@ -3,7 +3,7 @@ package config
 const (
 	Base_Folder = "github.com/kevinrizkhy/Internal/"
 	Base_Port   = ":8080"
-	Base_URL    = "www.pardev.id/" + Base_Port + "/"
+	Base_URL    = "127.0.0.1:" + Base_Port + "/"
 
 	WK_PATH = "F:/GoFILE/src/github.com/SebastiaanKlippert/wkhtmltopdf/bin/wkhtmltopdf.exe"
 )
