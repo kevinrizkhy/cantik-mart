b90baa372d819159112167825ffada55bf682111
kevinrizkhy <kevin.rizkhy85@gmail.com>
2018-08-16 14:37:01 +0700

    test gitignore2

--- config/db.go
+++ config/db.go
@@ -3,7 +3,7 @@ package config
 const (
 	DB_ADAPTER  = "postgres"
 	DB_USER     = "postgres"
-	DB_PASSWORD = "roots"
+	DB_PASSWORD = "root"
 	DB_NAME     = "cantik-mart"
 	DB_SSLMODE  = "disable"
 )
