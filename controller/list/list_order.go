a28f5cba949bbe92955a3298e63a479e1a40e4cd
alfianprabowo <alfian.prab@gmail.com>
2018-09-03 22:28:51 +0700

    Order table

--- controller/list/list_order.go
+++ controller/list/list_order.go
@@ -1,10 +1,11 @@
 package list
 
 import (
+	item "github.com/pardev/cantik-mart/model/item"
 	order "github.com/pardev/cantik-mart/model/order"
 	sessions "github.com/pardev/cantik-mart/model/session"
 	//store "github.com/pardev/cantik-mart/model/store"
-	//supplier "github.com/pardev/cantik-mart/model/supplier"
+	supplier "github.com/pardev/cantik-mart/model/supplier"
 	user "github.com/pardev/cantik-mart/model/user"
 	"html/template"
 	"net/http"
@@ -21,8 +22,8 @@ func ListOrder(w http.ResponseWriter, r *http.Request) {
 		)
 		data := user.GetUserDetail(id)
 		data["list_order"] = template.JS(order.GetOrder())
-		//data["list_supplier"] = template.JS(supplier.GetSupplierList())
-		//data["list_order"] = order.GetOrder()
+		data["list_items"] = template.JS(item.GetItemList())
+		data["list_supplier"] = supplier.GetSupplier()
 		//data["list_items_category"] = item.GetItemCategoryListArray()
 
 		/*data["list_user"] = template.JS(user.GetUserList())
